69fad7a789422b3ecc5ee4a31e7812f9a51bdb6f
Paul Dooner <paul.dooner@gmail.com>
2017-12-02 12:02:31 -0800

    5 commit

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Bonjour, World from COMP 3916!'
+    return 'Salut, World from COMP 3916!'
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
